d5210f4061ca5c785f62ccf8d480ea455d0998e1
Xujiayao <i@xujiayao.com>
2024-10-19 18:09:46 +0800

    使用Gradle

---
+++ src/main/java/com/xujiayao/bmw_brx_media_converter/Main.java
@@ -0,0 +1,9 @@
+package com.xujiayao.bmw_brx_media_converter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("中文字符测试");
+	}
+}
